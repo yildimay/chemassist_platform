93169a8449b8656201d769050a8658bcb11cab42
yildimay <yldry2526@gmail.com>
2025-04-02 18:08:28 +0300

    Create __init__.py

---
+++ gaussian/__init__.py
@@ -0,0 +1,6 @@
+# gaussian/__init__.py
+# This file makes the 'gaussian' folder a Python package.
+
+# You can optionally initialize shared variables or configs here if needed
+
+__all__ = ["gaussian_fixer_ui", "gaussian_input_ui"]
